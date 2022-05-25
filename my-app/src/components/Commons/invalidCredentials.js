e24dafdb897ae5be4513f728eb9deb8848ce0cbd
migsilvan2211 <migsilvan2211@gmail.com>
2022-05-25 09:52:47 +0800

    fixed invalid credentials

--- my-app/src/components/Commons/invalidCredentials.js
+++ my-app/src/components/Commons/invalidCredentials.js
@@ -1,8 +1,8 @@
 import Swal from 'sweetalert2'
 import Navigate from 'react-router-dom';
 
-export default function invalidCredentials()
-	if (localStorage.getItem("token")) {
+export default function invalidCredentials() {}
+	if (localStorage.getItem("token")) 
 		Swal.fire("Session Timed Out", "Please log in again", "warning");
 	else
 		Swal.fire("Session Timed Out", "Please log in again", "warning");
