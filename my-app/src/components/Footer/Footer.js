fe3f24add688ff18c1019048ed60c9ca52892877
migsilvan2211 <migsilvan2211@gmail.com>
2022-05-26 14:26:21 +0800

    fixing footer / added links to footer images

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Image} from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive';
 
 export default function Footer() {
@@ -8,11 +8,11 @@ export default function Footer() {
 		return(
 			<footer style={{backgroundColor: "papayawhip"}}>
 				<Row className="container-fluid d-flex p-0 m-0">
-					<Col md={1} className="d-flex align-items-center justify-content-end p-0 pe-0 m-0"  >
+					<Col className="d-flex align-items-center justify-content-end p-0 pe-0 m-0"  >
 						<h1 style={{fontSize: "90px", borderRightStyle: "solid"}}className="cardo pb-0 pe-2 mb-0">M</h1>
 					</Col>
 
-					<Col md={4} className="ps-0" >
+					<Col  className="ps-0" >
 						<Row className="p-0 m-0 pt-4">
 							<p style={{marginBottom: "0px"}}><span style={{fontSize: "30px"}} className="cardo p-0 m-0">Express</span><span className="cardo p-0 m-0"> yourself</span></p>
 						</Row>
@@ -22,11 +22,21 @@ export default function Footer() {
 						</Row>
 					</Col>
 
-					<Col md={4}>
+					<Col >
+						<Row>
+							<span className="p0 m-0 cardo">Follow Us:</span>
+						</Row>
 
+						<Row className="d-flex flex-row">
+							<a href="https://www.facebook.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Facebook_svg-512.png"/></a>
+							<a href="https://www.linkedin.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-512.png"/></a>
+							<a href="https://www.instagram.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-512.png"/></a>
+							<a href="https://www.youtube.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Youtube3_svg-512.png"/></a>
+							<a href="https://www.twitter.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Twitter5_svg-512.png"/></a>
+						</Row>
 					</Col>
 					
-					<Col md={2}>
+					<Col >
 
 					</Col>
 				</Row>
