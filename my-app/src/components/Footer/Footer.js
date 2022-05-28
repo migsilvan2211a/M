f215eebe1b51db08e9ccec858286f36cad6a59e4
migsilvan2211 <migsilvan2211@gmail.com>
2022-05-29 00:15:11 +0800

    pushing updates

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -6,40 +6,37 @@ export default function Footer() {
 	
 	if (!isSmall){
 		return(
-			<footer style={{backgroundColor: "papayawhip"}}>
-				<Row className="container-fluid d-flex p-0 m-0">
-					<Col className="d-flex align-items-center justify-content-end p-0 pe-0 m-0"  >
-						<h1 style={{fontSize: "90px", borderRightStyle: "solid"}}className="cardo pb-0 pe-2 mb-0">M</h1>
-					</Col>
+			<footer style={{backgroundColor: "papayawhip"}} className="py-5">
+				<div className="d-flex p-0 m-0 justify-content-center align-items-center flex-column">
+					<div className="d-inline-flex">
+						<div className=" p-0 pe-0 m-0 d-inline-block"  >
+							<span style={{fontSize: "90px", borderRightStyle: "solid"}} className="cardo pb-0 pe-2 mb-0 justify-self-end">M</span>
+						</div>
 
-					<Col  className="ps-0" >
-						<Row className="p-0 m-0 pt-4">
-							<p style={{marginBottom: "0px"}}><span style={{fontSize: "30px"}} className="cardo p-0 m-0">Express</span><span className="cardo p-0 m-0"> yourself</span></p>
-						</Row>
+						<div className="p-0 m-0 d-inline-block" >
+							<Row className="p-0 m-0 pt-4">
+								<div><span style={{fontSize: "30px"}} className="cardo p-0 m-0">Express</span><span className="cardo p-0 m-0"> yourself</span></div>
+							</Row>
 
-						<Row className="p-0 m-0 pb-4">
-							<p style={{marginBottom: "0px"}}><span className="cardo p-0 m-0">with</span><span style={{fontSize: "30px"}} className="cardo p-0 m-0"> Fashion</span></p>
-						</Row>
-					</Col>
-
-					<Col >
-						<Row>
-							<span className="p0 m-0 cardo">Follow Us:</span>
-						</Row>
-
-						<Row className="d-flex flex-row">
-							<a href="https://www.facebook.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Facebook_svg-512.png"/></a>
-							<a href="https://www.linkedin.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-512.png"/></a>
-							<a href="https://www.instagram.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-512.png"/></a>
-							<a href="https://www.youtube.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Youtube3_svg-512.png"/></a>
-							<a href="https://www.twitter.com"><Image style={{height: "50px", width:"50px", objectFit: "cover"}} className="p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Twitter5_svg-512.png"/></a>
-						</Row>
-					</Col>
+							<Row className="p-0 m-0 pb-4">
+								<div><span className="cardo p-0 m-0">with</span><span style={{fontSize: "30px"}} className="cardo p-0 m-0"> Fashion</span></div>
+							</Row>
+						</div>
+					</div>
+					<br />
+					<div className="d-inline-flex flex-column p-0 m-0 justify-content-center align-items-start">
+							<div style={{fontSize: "25px"}} className="p-0 m-0 cardo text-center align-self-center">Follow Us:</div>
+							<Row className="d-flex ">
+							<a className="col p-0 m-0" href="https://www.facebook.com"><Image style={{height: "40px", width:"40px", objectFit: "cover"}} className=" p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Facebook_svg-512.png"/></a>
+							<a className="col p-0 m-0" href="https://www.linkedin.com"><Image style={{height: "40px", width:"40px", objectFit: "cover"}} className=" p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-512.png"/></a>
+							<a className="col p-0 m-0" href="https://www.instagram.com"><Image style={{height: "40px", width:"40px", objectFit: "cover"}} className=" p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-512.png"/></a>
+							<a className="col p-0 m-0" href="https://www.youtube.com"><Image style={{height: "40px", width:"40px", objectFit: "cover"}} className=" p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Youtube3_svg-512.png"/></a>
+							<a className="col p-0 m-0" href="https://www.twitter.com"><Image style={{height: "40px", width:"40px", objectFit: "cover"}} className=" p-0 mx-1" src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Twitter5_svg-512.png"/></a>	
+							</Row>
+					</div>
 					
-					<Col >
 
-					</Col>
-				</Row>
+				</div>
 			</footer>
 		)
 	}
--- my-app/src/components/Products/ProductsCard.js
+++ my-app/src/components/Products/ProductsCard.js
@@ -7,8 +7,8 @@ export default function ProductsCard({name, description, price, stock, _id, img}
 	const clickHandle = () => navi(`/products/get/${_id}`)
 	let link = (img && img.link) ? img.link : "https://wallpaperaccess.com/full/4966661.jpg"
 	return(
-		<Card onClick={clickHandle} style={{height: "310px", cursor: "pointer", objectFit: "cover"}} className="p-0 m-2 cardWidth">
-			<Card.Img src={link} style={{height: "190px"}} className=" m-0 p-0 border cardWidth "/>
+		<Card onClick={clickHandle} style={{height: "310px", cursor: "pointer"}} className="p-0 m-2 cardWidth">
+			<Card.Img src={link} style={{height: "190px", objectFit: "cover"}} className=" m-0 p-0 border cardWidth "/>
 			<Card.Body className="cardHighlight m-0 p-0 cardWidth" >
 				<Card.Title className="  px-2 pt-2 pb-0 mb-0 cardWidth" style={{overflow: "hidden"}}>{name}</Card.Title>
 			</Card.Body>
