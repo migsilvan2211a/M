dbeb03b58f5581a4bf908a5a82becaac8c454c3f
migsilvan2211 <migsilvan2211@gmail.com>
2022-06-03 03:28:57 +0800

    github

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {useMediaQuery} from 'react-responsive';
 import HomeSmall from '../components/Home/HomeSmall'
 import HomeBig from '../components/Home/HomeBig'
+import AppNavbar from '../components/Navbar/AppNavbar';
 export default function Home() {
 	const isSmall = useMediaQuery({query: '(max-width: 768px)'})
 	console.log(isSmall)
 	return(
-		<div id="homePicture" className="homeBorder">
+		<div id="homePicture">
 			
-				{(isSmall) ? <HomeSmall /> : <HomeBig />}
+				<AppNavbar />
 			
 		</div>
 	);
